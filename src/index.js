fa89b14b78d527811f776956ad7de5e3237be735
AceAltair13 <tirththoria13@hotmail.com>
2021-02-28 16:43:10 +0530

    Create static branch for deployment

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
-import Footer from "./components/Footer";
-
-ReactDOM.render(
-    [<App key="0" />, <Footer key="1" />],
-    document.getElementById("root")
-);
